ecff7c155b7507ecb6ac9292d626ce824a1f7903
hoot-dev <mhootman1980@gmail.com>
2022-04-24 13:09:51 -0500

    customized admin site

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import (
+    Book, Publisher, Contributor, BookContributor, Review
+)
+
+admin.site.register(Publisher)
+admin.site.register(Book)
+admin.site.register(Contributor)
+admin.site.register(BookContributor)
+admin.site.register(Review)
