a0f6e4aee6c83e7370c93b4e406eb571315d98eb
hoot-dev <mhootman1980@gmail.com>
2022-04-23 20:13:25 -0500

    exercise 1.0.3 first view and url

--- bookr/urls.py
+++ bookr/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from reviews import views as review_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', review_views.index),
 ]
