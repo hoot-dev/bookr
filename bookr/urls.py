7fdebd240feb9e089d027150b0ce046327aed771
hoot-dev <mhootman1980@gmail.com>
2022-04-28 23:01:01 -0500

    adding user login

--- bookr/urls.py
+++ bookr/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('accounts/', include(('django.contrib.auth.urls', 'auth'), namespace='accounts')),
     path('admin/', admin.site.urls),
     path('', include('reviews.urls')),
 ]
