d3fab7e63428aaa10f9364382b99028636679c3a
hoot-dev <mhootman1980@gmail.com>
2022-05-06 10:29:15 -0500

    all books view with DRF

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from . import api_views
 
 urlpatterns = [
+    path('api/all_books/', api_views.all_books, name='all_books'),
     path('', views.index, name='index'),
     path('books/', views.book_list, name='book_list'),
     path('books/<int:pk>', views.book_detail, name='book_detail'),
