e613259bde031a70890fb7a47e49a1b4a7651a81
hoot-dev <mhootman1980@gmail.com>
2022-05-07 10:56:35 -0500

    using react to fetch book list and display it

--- static/react-example.js
+++ static/react-example.js
@@ -12,4 +12,44 @@ class ClickCounter extends React.Component {
         </button>;
     }
 }
-ReactDOM.render(<ClickCounter/>, document.getElementById('react_container'));
+
+class BookDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { books: [], fetchInProgress: false };
+    }
+
+    doFetch() {
+        if (this.state.fetchInProgress)
+            return;
+
+        this.setState({ fetchInProgress: true })
+
+        fetch(this.props.url, {
+            method: 'GET',
+            headers: {
+                    Accept: 'application/json'
+                }
+            }
+        ).then((response) => {
+            return response.json();
+        }).then((data) => {
+            this.setState({ fetchInProgress: false, books: data })
+        })
+    }
+
+    render() {
+        const bookListItems = this.state.books.map((book) => {
+            return <li key={ book.pk }>{ book.title }</li>;
+        })
+
+        const buttonText = this.state.fetchInProgress  ? 'Fetch in Progress' : 'Fetch';
+
+        return <div>
+            <ul>{ bookListItems }</ul>
+            <button onClick={ () => this.doFetch() } disabled={ this.state.fetchInProgress }>
+                {buttonText}
+            </button>
+        </div>;
+    }
+}
